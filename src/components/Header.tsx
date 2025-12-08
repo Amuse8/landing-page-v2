112f75cb938dd483bb7486710d1dd3e0d8d9c22d
glojuw013 <nevertheless113@naver.com>
2025-12-08 18:03:09 +0900

    feat: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,17 +2,44 @@ import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
     return (
-        <header className="header">
-        <div className="header-inner">
-            <Link to="/" className="logo">
-            회사
-            </Link>
-            <nav className="nav">
-            <NavLink to="/about">회사소개</NavLink>
-            <NavLink to="/services">서비스</NavLink>
-            <NavLink to="/custom-ai">Custom AI</NavLink>
-            </nav>
-        </div>
+        <header className="header flex">
+            <div className="header-inner flex">
+                <Link to="/" className="logo">
+                    회사
+                </Link>
+                <nav className="nav flex items-center space-x-10">
+                    <NavLink
+                        to="/about"
+                        className={({ isActive }) =>
+                            `hover-link ${
+                                isActive ? "active" : ""
+                            }`
+                        }
+                    >
+                        회사소개
+                    </NavLink>
+                    <NavLink
+                        to="/services"
+                        className={({ isActive }) =>
+                            `hover-link ${
+                                isActive ? "active" : ""
+                            }`
+                        }
+                    >
+                        서비스
+                    </NavLink>
+                    <NavLink
+                        to="/custom-ai"
+                        className={({ isActive }) =>
+                            `hover-link ${
+                                isActive ? "active" : ""
+                            }`
+                        }
+                    >
+                        Custom AI
+                    </NavLink>
+                </nav>
+            </div>
         </header>
     );
 };
